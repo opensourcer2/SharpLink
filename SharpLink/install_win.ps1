08106dcf9999108141e3a60e4384ffc049ea3d7a
opensourcer2 <>
2019-04-22 00:02:48 -0500

    Add powershell script for installation on Windows

---
+++ SharpLink/install_win.ps1
@@ -0,0 +1,11 @@
+﻿New-Item -ItemType Directory -Force -Path C:\ToxTunnel
+
+if(!$PSScriptRoot){ $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
+
+Copy-Item -Force -Path $PSScriptRoot\* -Destination C:\ToxTunnel\
+
+$PATH = [Environment]::GetEnvironmentVariable("PATH")
+$toxtunnel_path = "C:\ToxTunnel"
+if( $PATH -notlike "*"+$toxtunnel_path+"*" ){
+    [Environment]::SetEnvironmentVariable("PATH", "$PATH;$toxtunnel_path", "Machine")
+}
